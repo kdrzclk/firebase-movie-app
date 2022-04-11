83a0d58ee919ad9c40ba9b317685ec7655315c77
kdrzclk <kadir.ozcelik16@gmail.com>
2022-04-11 22:05:12 +0300

    navbar button oluşturuldu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  // const currentUser = {
+  //   displayName: "Kadir Özçelik",
+  // };
+
+  const currentUser = false;
   return (
     <div>
       <nav className="navbar navbar-expand-lg">
@@ -9,6 +14,21 @@ const Navbar = () => {
           <Link to={"/"} className="navbar-brand text-white">
             <h4>React Movie App</h4>
           </Link>
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <h5 className="mb-0 text-capitalize">
+                {currentUser?.displayName}
+              </h5>
+            ) : (
+              <button className="ms-2 btn btn-outline-light">LogIn</button>
+            )}
+
+            {currentUser ? (
+              <button className="ms-2 btn btn-outline-light">Logout</button>
+            ) : (
+              <button className="ms-2 btn btn-outline-light">Register</button>
+            )}
+          </div>
         </div>
       </nav>
     </div>
