3de75022f9e3c51ce9ff435d9c730a737720185a
kdrzclk <kadir.ozcelik16@gmail.com>
2022-04-11 21:52:55 +0300

    navbar ilk kısmı tamamlandı

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand text-white">
+            <h4>React Movie App</h4>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
